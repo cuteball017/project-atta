c715335258c2112a5c9cd10109f14e4126f297b1
Kazuki Takahashi <narita30830613@gmail.com>
2024-08-06 18:38:46 +0900

    add search box

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,35 +1,46 @@
+"use client";
+import { useState } from "react";
 import Image from "next/image";
 import styles from "./index.module.css";
 import NavBar from "@/components/navBar/navBar";
 import SearchInput from "@/components/searchInput/searchInput";
 
 export default function Home() {
+  const [showUI, setShowUI] = useState(false);
   return (
     <>
       <div className={styles.container}>
-        <SearchInput />
-        <div className={styles.register}>
-          <p>取得物を登録する</p>
-          <Image src="/camera-white.svg" alt="camera" width={45} height={45} />
-        </div>
+        <SearchInput showUI={showUI} setShowUI={setShowUI} />
 
-        <div className={styles.recievedSchedule}>
-          <h1 className={styles.heading}>本日のお渡し予定</h1>
-          <ul>
-            <li>No.10 財布</li>
-            <li>No.1 時計</li>
-          </ul>
-        </div>
-
-        <div className={styles.achievementRate}>
-          <h1 className={styles.heading}>今月の取得物</h1>
-          <div className={styles.forCenter}>
-            <div className={styles.circle}>
-              <p className={styles.rate}>50%</p>
-              <p className={styles.total}>1000件</p>
+        {!showUI && (
+          <>
+            <div className={styles.register}>
+              <p>取得物を登録する</p>
+              <Image
+                src="/camera-white.svg"
+                alt="camera"
+                width={45}
+                height={45}
+              />
             </div>
-          </div>
-        </div>
+            <div className={styles.recievedSchedule}>
+              <h1 className={styles.heading}>本日のお渡し予定</h1>
+              <ul>
+                <li>No.10 財布</li>
+                <li>No.1 時計</li>
+              </ul>
+            </div>
+            <div className={styles.achievementRate}>
+              <h1 className={styles.heading}>今月の取得物</h1>
+              <div className={styles.forCenter}>
+                <div className={styles.circle}>
+                  <p className={styles.rate}>50%</p>
+                  <p className={styles.total}>1000件</p>
+                </div>
+              </div>
+            </div>
+          </>
+        )}
       </div>
 
       <NavBar />
--- src/components/searchInput/searchInput.tsx
+++ src/components/searchInput/searchInput.tsx
@@ -1,16 +1,57 @@
 import Image from "next/image";
 
-import styles from "./index.module.css"
+import styles from "./index.module.css";
 
-function SearchInput() {
+function SearchInput({
+  showUI,
+  setShowUI,
+}: {
+  showUI: boolean;
+  setShowUI: React.Dispatch<React.SetStateAction<boolean>>;
+}) {
+  const handleFocus = () => {
+    setShowUI(true);
+  };
+
+  const handleBlur = () => {
+    setShowUI(false);
+  };
   return (
     <>
       <div className={styles.searchInput}>
         <label>
           <Image src="/search.png" alt="search" width={45} height={45} />
         </label>
-        <input type="text" placeholder="取得物をさがす"></input>
+        <input
+          type="text"
+          placeholder="取得物をさがす"
+          onFocus={handleFocus}
+          onBlur={handleBlur}
+        ></input>
       </div>
+
+      {showUI && (
+        <div className={styles.additionalUI}>
+          <div className={styles.category}>
+            <h1 className={styles.heading}>カテゴリーを選択</h1>
+            <ul>
+              <li>財布</li>
+              <li>時計</li>
+              <li>眼鏡</li>
+              <li>携帯電話</li>
+              <li>その他</li>
+            </ul>
+          </div>
+
+          <div className={styles.hotItems}>
+            <h1 className={styles.heading}>よく調べられてるもの</h1>
+            <ul>
+                <li>ハンカチ</li>
+                <li>イヤホン</li>
+            </ul>
+          </div>
+        </div>
+      )}
     </>
   );
 }
