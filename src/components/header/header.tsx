342f63f0da31794f7cd119d94ac4c91cd7d2e896
Kazuki Takahashi <narita30830613@gmail.com>
2024-08-06 20:50:36 +0900

    add Link

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,13 +1,16 @@
 import Image from "next/image";
+import Link from "next/link";
 import styles from "./index.module.css";
 
 function Header() {
-    return (
-        <div className={styles.header}>
-            <h1 className={styles.logo}>Atta</h1>
-            <Image src="/gear.svg" alt="gear" width={32} height={32} />
-        </div>
-    );
+  return (
+    <div className={styles.header}>
+      <Link href="/">
+        <h1 className={styles.logo}>Atta</h1>
+      </Link>
+      <Image src="/gear.svg" alt="gear" width={32} height={32} />
+    </div>
+  );
 }
 
 export default Header;
--- src/components/navBar/navBar.tsx
+++ src/components/navBar/navBar.tsx
@@ -1,34 +1,44 @@
 import Image from "next/image";
+import Link from "next/link";
 
-import styles from "./index.module.css"
+import styles from "./index.module.css";
 
 function NavBar() {
-    return (
-        <div className={styles.navBar}>
-            <ul>
-                <li>
-                    <Image src="/home.svg" alt="home" width={45} height={45} />
-                    <div>ホーム</div>
-                </li>
-                <li>
-                    <Image src="/search.png" alt="search" width={45} height={45} />
-                    <div>検索</div>
-                </li>
-                <li>
-                    <Image src="/camera.svg" alt="camera" width={45} height={45} />
-                    <div>登録</div>
-                </li>
-                <li>
-                    <Image src="/list.svg" alt="list" width={45} height={45} />
-                    <div>一覧</div>
-                </li>
-                <li>
-                    <Image src="/notification.svg" alt="notification" width={45} height={45} />
-                    <div>通知</div>
-                </li>
-            </ul>
-        </div>
-    );
+  return (
+    <div className={styles.navBar}>
+      <ul>
+        <li>
+          <Link href="/">
+            <Image src="/home.svg" alt="home" width={45} height={45} />
+            <div>ホーム</div>
+          </Link>
+        </li>
+        <li>
+          <Link href="/search">
+            <Image src="/search.png" alt="search" width={45} height={45} />
+            <div>検索</div>
+          </Link>
+        </li>
+        <li>
+          <Image src="/camera.svg" alt="camera" width={45} height={45} />
+          <div>登録</div>
+        </li>
+        <li>
+          <Image src="/list.svg" alt="list" width={45} height={45} />
+          <div>一覧</div>
+        </li>
+        <li>
+          <Image
+            src="/notification.svg"
+            alt="notification"
+            width={45}
+            height={45}
+          />
+          <div>通知</div>
+        </li>
+      </ul>
+    </div>
+  );
 }
 
 export default NavBar;
