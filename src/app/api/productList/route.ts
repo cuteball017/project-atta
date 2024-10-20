6fbe3f4982327f145ab3c7e6e56d33f0d0c20b48
Kazuki Takahashi <narita30830613@gmail.com>
2024-10-20 11:56:44 +0900

    add no store

--- src/app/api/productList/route.ts
+++ src/app/api/productList/route.ts
@@ -1,6 +1,7 @@
 import { NextResponse } from "next/server";
 import { supabase } from "@/utils/supabase";
 
+export const fetchCache = 'force-no-store'
 
 export async function GET(req: Request) {
     const { data, error } = await supabase.from("lost_items").select();
